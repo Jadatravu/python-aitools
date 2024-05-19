219904c91b2936e81bb679a4e5e41c475ef447b3
Jadatravu <surya.janardhan@gmail.com>
2024-05-19 21:54:43 +0530

    Add files via upload

---
+++ m1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def my_function():
+    print("this is in module")
+
+def my_new_function():
+    print("this is another function in my module")
---
+++ t1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# this is user defined python module
+
+d1={100:"apple",200:"banana",300:"google",400:"chrome"}
+l1 = [ 12,45,67,89,90]
+
+str1 = "Today is sunday we are learning python modules"
+
+def count_the_length_of_the_string(str2):
+    l2=list(str2)
+    result= len(l2)
+    return result
+
+
+# In[ ]:
+
+
+
+
